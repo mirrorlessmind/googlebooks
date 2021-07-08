76673ce625aeb8975c11507a5e2f8c86c5e51ec5
Cigi Tipton <cigi@mirrorlessmind.com>
2021-07-08 18:20:47 -0500

    Updated Server, api

--- client/src/utils/GoogleBooksAPI.js
+++ client/src/utils/GoogleBooksAPI.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=search+terms";
-const APIKEY = "AIzaSyBlGFX-s2RcnODfNYpv9hE-bAAPsexNTpU";
+
 
 export default {
     search: function(query) {
         let q = (query).split(" ").join("+");
-        return axios.get(BASEURL + q + APIKEY)
+        const res = axios.get(BASEURL + q + '&filter=partial&callback=handleResults&maxResults=1');
+        return res;
     }
 };
